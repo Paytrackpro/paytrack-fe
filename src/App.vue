36634c25296e0a6ef2886d2be93c1034148b1bba
JustinDo <justindo.works@gmail.com>
2023-01-05 23:46:14 +0700

    init project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-view />
+</template>
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
+
+<style>
+.bg-image {
+  background-image: linear-gradient(135deg, #7028e4 0%, #e5b2ca 100%);
+}
+</style>
