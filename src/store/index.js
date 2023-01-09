eb6a838bafa72e8e65b0577f5d3d88f22e91e381
Justin Do <justindo.works@gmail.com>
2023-01-10 01:18:49 +0700

    add auth store

--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 // we first import the module
-import login from './login'
+import auth from './auth'
 
 Vue.use(Vuex)
 
 export default function (/* { ssrContext } */) {
   const Store = new Vuex.Store({
     modules: {
-      login
+      auth
     },
 
     // enable strict mode (adds overhead!)
