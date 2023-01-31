c763349395f59986b6aab8801017e298f9b069db
Justin Do <justindo.works@gmail.com>
2023-01-31 23:04:44 +0700

    update login screen

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -2,46 +2,60 @@
   <q-layout>
     <q-page-container>
       <q-page>
-        <div v-bind:style="{ maxWidth: '28rem' }" class="flex flex-center q-mx-auto window-height">
-          <div class="column justify-center q-pa-xl col-grow">
-            <div class="flex items-center q-mb-md q-gutter-sm text-grey-7">
-              <img v-bind:style="{ height: '2em' }" src="~assets/team_raedah.svg" />
-              <span class="text-bold">MGMT</span>
-            </div>
-            <q-card class="col" flat bordered>
-              <q-card-section class="q-pa-lg">
-                <q-form class="q-gutter-y-md">
-                  <div>
-                    <p class="q-mt-none q-mb-xs text-weight-medium">User name</p>
-                    <q-input v-model="username" placeholder="Your user name" outlined dense lazy-rules stack-label>
-                    </q-input>
-                  </div>
-                  <div>
-                    <p class="q-mt-none q-mb-xs text-weight-medium">Password</p>
-                    <q-input
-                      :type="isPwd ? 'password' : 'text'"
-                      v-model="password"
-                      outlined
-                      dense
-                      placeholder="Password"
-                      lazy-rules
-                      stack-label
-                    >
-                      <template v-slot:append>
-                        <q-icon
-                          :name="isPwd ? 'visibility_off' : 'visibility'"
-                          class="cursor-pointer"
-                          @click="isPwd = !isPwd"
-                        />
-                      </template>
-                    </q-input>
-                  </div>
-                  <q-btn label="Sign in" to="/" type="submit" color="primary" class="full-width" @click="login" />
-                </q-form>
-              </q-card-section>
-            </q-card>
-            <div class="text-center text-grey-6 q-mt-md">
-              Don't have account yet? <a class="link text-light-blue-6" href="/apply">Apply now</a>
+        <div class="position-relative">
+          <img class="absolute q-ma-md" v-bind:style="{ height: '2em' }" src="~assets/team_raedah.svg" />
+          <div v-bind:style="{ maxWidth: '28rem' }" class="flex flex-center q-mx-auto window-height">
+            <div class="column justify-center q-pa-xl col-grow">
+              <div class="flex items-center q-mb-md q-gutter-sm text-grey-7">
+                <span class="text-bold">Login</span>
+              </div>
+              <q-card class="col" flat bordered>
+                <q-card-section class="q-pa-lg">
+                  <q-form class="q-gutter-y-md" @submit="login">
+                    <div>
+                      <p class="q-mt-none q-mb-xs text-weight-medium">User name</p>
+                      <q-input
+                        v-model="username"
+                        placeholder="Your user name"
+                        outlined
+                        dense
+                        lazy-rules
+                        stack-label
+                        hide-bottom-space
+                        :rules="[(val) => (val && val.length > 0) || 'Please enter your username']"
+                      >
+                      </q-input>
+                    </div>
+                    <div>
+                      <p class="q-mt-none q-mb-xs text-weight-medium">Password</p>
+                      <q-input
+                        :type="isPwd ? 'password' : 'text'"
+                        v-model="password"
+                        outlined
+                        dense
+                        placeholder="Password"
+                        lazy-rules
+                        stack-label
+                        hide-bottom-space
+                        :rules="[(val) => (val && val.length > 0) || 'Please enter your password']"
+                      >
+                        <template v-slot:append>
+                          <q-icon
+                            :name="isPwd ? 'visibility_off' : 'visibility'"
+                            class="cursor-pointer"
+                            @click="isPwd = !isPwd"
+                          />
+                        </template>
+                      </q-input>
+                    </div>
+                    <p v-if="error" class="q-mb-none text-red">{{ error }}</p>
+                    <q-btn label="Sign in" type="submit" color="primary" class="full-width" />
+                  </q-form>
+                </q-card-section>
+              </q-card>
+              <div class="text-center text-grey-6 q-mt-md">
+                Don't have account yet? <a class="link text-light-blue-6" href="/apply">Apply now</a>
+              </div>
             </div>
           </div>
         </div>
@@ -56,15 +70,24 @@ export default {
     return {
       username: "",
       password: "",
+      error: null,
       isPwd: true,
     }
   },
   methods: {
     login() {
-      this.$store.dispatch("auth/login", {
-        username: this.username,
-        password: this.password,
-      })
+      this.$store
+        .dispatch("auth/login", {
+          username: this.username,
+          password: this.password,
+        })
+        .then((res) => {
+          this.$router.push({ name: "Home" })
+        })
+        .catch((error) => {
+          this.error = error.response ? error.response.data.message : error.message
+        })
+
     },
   },
 }
