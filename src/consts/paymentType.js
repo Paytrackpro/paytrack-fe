2bebe23b03944bb38077d1c7fffbbbc46ea3a0b8
Justin Do <justindo.works@gmail.com>
2023-02-06 03:27:33 +0700

    user list api integration

---
+++ src/consts/paymentType.js
@@ -0,0 +1,5 @@
+export default {
+  BTC: 1,
+  LTC: 2,
+  DCR: 3
+}
--- src/pages/users/list.vue
+++ src/pages/users/list.vue
@@ -30,6 +30,9 @@
 </template>
 
 <script>
+import paymentType from "../../consts/paymentType"
+import { date } from "quasar"
+
 export default {
   data() {
     return {
@@ -42,187 +45,58 @@ export default {
       },
       columns: [
         {
-          name: "userName",
+          name: "user_name",
           required: true,
           label: "User Name",
           align: "center",
-          field: (row) => row.userName,
+          field: (row) => row.user_name,
           format: (val) => `${val}`,
           sortable: true,
         },
-        { name: "displayName", align: "center", label: "Dislay Name", field: "displayName", sortable: true },
+        { name: "display_name", align: "center", label: "Dislay Name", field: "display_name", sortable: true },
         { name: "email", align: "center", label: "Email", field: "email", sortable: true },
-        { name: "paymentType", align: "center", label: "Payment Type", field: "paymentType" },
-        { name: "createdAt", align: "center", label: "Created At", field: "createdAt" },
-        { name: "lastSeen", align: "center", label: "Last Seen", field: "lastSeen" },
-      ],
-      rows: [
         {
-          userName: "Luella Dillon",
-          displayName: "Beck",
-          email: "beckdillon@medcom.com",
-          paymentType: "BTC",
-          createdAt: "26/11/2016",
-          lastSeen: "23/08/2015",
+          name: "payment_type",
+          align: "center",
+          label: "Payment Type",
+          field: "payment_type",
+          format: (val) => {
+            if (val == paymentType.BTC) return "BTC"
+            if (val == paymentType.LTC) return "LTC"
+            if (val == paymentType.DCR) return "DCR"
+          },
         },
         {
-          userName: "Jamie Knox",
-          displayName: "Maria",
-          email: "mariaknox@medcom.com",
-          paymentType: "BTC",
-          createdAt: "13/09/2014",
-          lastSeen: "30/08/2014",
+          name: "created_at",
+          align: "center",
+          label: "Created At",
+          field: "created_at",
+          format: (val) => date.formatDate(val, "DD/MM/YYYY"),
         },
         {
-          userName: "Cummings Raymond",
-          displayName: "Cecile",
-          email: "cecileraymond@medcom.com",
-          paymentType: "BTC",
-          createdAt: "29/08/2014",
-          lastSeen: "01/05/2018",
-        },
-        {
-          userName: "Ashlee Travis",
-          displayName: "Carly",
-          email: "carlytravis@medcom.com",
-          paymentType: "BTC",
-          createdAt: "10/10/2018",
-          lastSeen: "23/02/2017",
-        },
-        {
-          userName: "Barton Bryan",
-          displayName: "Juliet",
-          email: "julietbryan@medcom.com",
-          paymentType: "BTC",
-          createdAt: "06/06/2016",
-          lastSeen: "24/04/2017",
-        },
-        {
-          userName: "Leanna Wells",
-          displayName: "Summer",
-          email: "summerwells@medcom.com",
-          paymentType: "BTC",
-          createdAt: "01/07/2020",
-          lastSeen: "25/11/2018",
-        },
-        {
-          userName: "Long Rogers",
-          displayName: "Amelia",
-          email: "ameliarogers@medcom.com",
-          paymentType: "BTC",
-          createdAt: "23/10/2019",
-          lastSeen: "28/09/2018",
-        },
-        {
-          userName: "Pearl Vargas",
-          displayName: "Camacho",
-          email: "camachovargas@medcom.com",
-          paymentType: "BTC",
-          createdAt: "14/09/2022",
-          lastSeen: "19/08/2016",
-        },
-        {
-          userName: "Pacheco Richards",
-          displayName: "Goldie",
-          email: "goldierichards@medcom.com",
-          paymentType: "BTC",
-          createdAt: "10/12/2019",
-          lastSeen: "25/05/2014",
-        },
-        {
-          userName: "Mullins Wolf",
-          displayName: "Noreen",
-          email: "noreenwolf@medcom.com",
-          paymentType: "BTC",
-          createdAt: "18/12/2014",
-          lastSeen: "16/03/2019",
-        },
-        {
-          userName: "Ewing Mcfarland",
-          displayName: "Bush",
-          email: "bushmcfarland@medcom.com",
-          paymentType: "BTC",
-          createdAt: "25/12/2020",
-          lastSeen: "13/01/2023",
-        },
-        {
-          userName: "Cooper Mcpherson",
-          displayName: "Tina",
-          email: "tinamcpherson@medcom.com",
-          paymentType: "BTC",
-          createdAt: "31/01/2021",
-          lastSeen: "17/12/2019",
-        },
-        {
-          userName: "Cleveland Garza",
-          displayName: "Lelia",
-          email: "leliagarza@medcom.com",
-          paymentType: "BTC",
-          createdAt: "07/08/2019",
-          lastSeen: "13/02/2015",
-        },
-        {
-          userName: "Montoya Riley",
-          displayName: "Eve",
-          email: "everiley@medcom.com",
-          paymentType: "BTC",
-          createdAt: "30/12/2016",
-          lastSeen: "16/05/2021",
-        },
-        {
-          userName: "Hope Greene",
-          displayName: "Parks",
-          email: "parksgreene@medcom.com",
-          paymentType: "BTC",
-          createdAt: "12/01/2018",
-          lastSeen: "11/09/2020",
-        },
-        {
-          userName: "Arline Francis",
-          displayName: "Carolina",
-          email: "carolinafrancis@medcom.com",
-          paymentType: "BTC",
-          createdAt: "12/04/2017",
-          lastSeen: "03/03/2021",
-        },
-        {
-          userName: "Ronda Sharp",
-          displayName: "Melba",
-          email: "melbasharp@medcom.com",
-          paymentType: "BTC",
-          createdAt: "27/09/2020",
-          lastSeen: "22/08/2021",
-        },
-        {
-          userName: "Keller Bray",
-          displayName: "Gardner",
-          email: "gardnerbray@medcom.com",
-          paymentType: "BTC",
-          createdAt: "27/07/2019",
-          lastSeen: "17/05/2018",
-        },
-        {
-          userName: "Arnold Erickson",
-          displayName: "Stark",
-          email: "starkerickson@medcom.com",
-          paymentType: "BTC",
-          createdAt: "19/08/2015",
-          lastSeen: "23/02/2020",
-        },
-        {
-          userName: "Shannon Dorsey",
-          displayName: "Jewell",
-          email: "jewelldorsey@medcom.com",
-          paymentType: "BTC",
-          createdAt: "12/08/2021",
-          lastSeen: "29/06/2014",
+          name: "last_seen",
+          align: "center",
+          label: "Last Seen",
+          field: "last_seen",
+          format: (val) => date.formatDate(val, "DD/MM/YYYY"),
         },
       ],
+      rows: [],
     }
   },
   computed: {
     pagesNumber() {
       return Math.ceil(this.rows.length / this.pagination.rowsPerPage)
+    },
+  },
+  created: function () {
+    this.getUserList()
+  },
+  methods: {
+    async getUserList() {
+      this.$api.get("/admin/user/list").then((res) => {
+        this.rows = res.data.data
+      })
     },
   },
 }
