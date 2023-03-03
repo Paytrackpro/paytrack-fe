c8b03428a0b2bc73460df9cf4d046b66dc720a9c
Justin Do <justindo.works@gmail.com>
2023-03-04 03:51:40 +0700

    generate public key credentials

--- src/pages/users/settings.vue
+++ src/pages/users/settings.vue
@@ -1,17 +1,37 @@
 <template>
   <q-card flat>
-    <q-toggle v-model="use2FA" color="green" label="Use two-factor authentication" />
+    <q-toggle
+      v-model="use2FA"
+      color="green"
+      label="Use two-factor authentication"
+    />
+    <q-toggle v-model="usePasskey" color="green" label="Use passkey" />
     <q-dialog v-model="dialog" no-backdrop-dismiss no-esc-dismiss>
       <q-card>
         <q-form @submit="login">
           <q-card-section>
-            <div class="text-h6">Please confirm your password to active two-factor authentication</div>
+            <div class="text-h6">
+              Please confirm your password to active two-factor authentication
+            </div>
             <q-input v-model="password" type="password" />
           </q-card-section>
 
           <q-card-actions align="right">
-            <q-btn flat label="Cancel" color="primary" type="button" @click="hideDialog" />
-            <q-btn flat label="Confirm" color="primary" type="submit" :loading="loading" :disable="loading" />
+            <q-btn
+              flat
+              label="Cancel"
+              color="primary"
+              type="button"
+              @click="hideDialog"
+            />
+            <q-btn
+              flat
+              label="Confirm"
+              color="primary"
+              type="submit"
+              :loading="loading"
+              :disable="loading"
+            />
           </q-card-actions>
         </q-form>
       </q-card>
@@ -20,13 +40,28 @@
       <q-card>
         <q-form @submit="disableOtp">
           <q-card-section>
-            <div class="text-h6">Please confirm your OTP to disable this function</div>
+            <div class="text-h6">
+              Please confirm your OTP to disable this function
+            </div>
             <q-input v-model="otp" type="text" />
           </q-card-section>
 
           <q-card-actions align="right">
-            <q-btn flat label="Cancel" color="primary" type="button" @click="hideOtpDialog" />
-            <q-btn flat label="Confirm" color="primary" type="submit" :loading="loading" :disable="loading" />
+            <q-btn
+              flat
+              label="Cancel"
+              color="primary"
+              type="button"
+              @click="hideOtpDialog"
+            />
+            <q-btn
+              flat
+              label="Confirm"
+              color="primary"
+              type="submit"
+              :loading="loading"
+              :disable="loading"
+            />
           </q-card-actions>
         </q-form>
       </q-card>
@@ -35,67 +70,106 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex"
+import { mapGetters } from "vuex";
 export default {
   name: "userSetting",
   data() {
     return {
       use2FA: false,
+      usePasskey: false,
       dialog: false,
       qrImage: null,
       password: "",
       loading: false,
       otpDialog: false,
       otp: null,
-    }
+    };
   },
   computed: {
     ...mapGetters({
       user: "auth/getUser",
     }),
   },
+  watch: {
+    usePasskey(val) {
+      if (val) {
+        this.enablePasskey();
+      }
+    },
+  },
   created() {
-    this.use2FA = this.user.otp
+    this.use2FA = this.user.otp;
     this.$watch("use2FA", (val) => {
       if (val != this.user.otp) {
-        if (val) this.dialog = true
-        else this.otpDialog = true
+        if (val) this.dialog = true;
+        else this.otpDialog = true;
       }
-    })
+    });
   },
   methods: {
     login() {
-      this.loading = true
+      this.loading = true;
       this.$api
         .post("/auth/login", {
           username: this.user.userName,
           password: this.password,
         })
         .then((res) => {
-          this.$router.push("settings/2fa")
+          this.$router.push("settings/2fa");
         })
         .finally(() => {
-          this.loading = false
-        })
+          this.loading = false;
+        });
     },
     async disableOtp(val) {
       await this.$store.dispatch("user/disableOtp", {
         otp: this.otp,
-      })
-      this.otpDialog = false
+      });
+      this.otpDialog = false;
       this.$store.commit("auth/setUser", {
         ...this.user,
         otp: false,
-      })
+      });
     },
     hideDialog() {
-      this.use2FA = !this.use2FA
-      this.dialog = false
+      this.use2FA = !this.use2FA;
+      this.dialog = false;
     },
     hideOtpDialog() {
-      this.use2FA = !this.use2FA
-      this.otpDialog = false
+      this.use2FA = !this.use2FA;
+      this.otpDialog = false;
+    },
+    async enablePasskey() {
+      const options = {
+        // TODO: replace with random string from server
+        challenge: new Uint8Array(26),
+        rp: {
+          name: "MGMT",
+        },
+        // TODO: replace with actual user info
+        user: {
+          id: Uint8Array.from("userId", (c) => c.charCodeAt(0)),
+          name: "testmgmt@mail.com",
+          displayName: "testmgmt",
+        },
+
+        pubKeyCredParams: [
+          { alg: -257, type: "public-key" },
+          // { alg: -7, type: "public-key" },
+        ],
+        // authenticatorSelection: {
+        //   authenticatorAttachment: "cross-platform",
+        // },
+        timeout: 60000,
+        attestation: "direct",
+      };
+
+      const credential = await navigator.credentials.create({
+        publicKey: options,
+      });
+
+      console.log(credential);
     },
   },
-}
+};
 </script>
