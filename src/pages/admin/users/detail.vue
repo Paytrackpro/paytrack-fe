14487da818b69d9973e9628427c61982c6be5bb0
Justin Do <justin.namdo@gmail.com>
2023-04-26 00:51:29 +0700

    Change the user's attribute and update function from the admin side

--- src/pages/admin/users/detail.vue
+++ src/pages/admin/users/detail.vue
@@ -2,13 +2,21 @@
   <q-card flat bordered class="q-pa-md">
     <q-form @submit="updateUserInfo">
       <div class="row q-mb-md q-col-gutter-md">
-        <div class="col-12 col-md-9">
+        <div class="col-12 col-sm-6 col-lg-4">
           <p class="q-mt-none q-mb-xs text-weight-medium">User Name</p>
-          <p class="q-mt-none q-mb-xs">{{ userName }}</p>
+          <q-input
+            v-model="userName"
+            placeholder="Username"
+            outlined
+            dense
+            lazy-rules
+            stack-label
+            :rules="[(val) => (val && val.length > 0) || 'Please enter username']"
+            hide-bottom-space
+          >
+          </q-input>
         </div>
-      </div>
-      <div class="row q-mb-md q-col-gutter-md">
-        <div class="col-4 col-md-3">
+        <div class="col-12 col-sm-6 col-lg-4">
           <p class="q-mt-none q-mb-xs text-weight-medium">Display Name</p>
           <q-input
             v-model="displayName"
@@ -21,45 +29,64 @@
           >
           </q-input>
         </div>
-        <div class="col-4 col-md-3">
-          <p class="q-mt-none q-mb-xs text-weight-medium">Email</p>
-          <q-input v-model="email" placeholder="Email" outlined dense lazy-rules stack-label hide-bottom-space>
-          </q-input>
-        </div>
-        <div class="col-4 col-md-3">
-          <p class="q-mt-none q-mb-xs text-weight-medium">Payment Type</p>
-          <q-select
-            v-model="paymentType"
-            :options="paymentTypeOptions"
-            placeholder="Payment Type"
-            option-value="value"
-            option-label="label"
-            emit-value
-            map-options
-            outlined
-            dense
-            stack-label
-            hide-bottom-space
-          >
-          </q-select>
-        </div>
       </div>
-      <div class="row q-mb-lg q-col-gutter-md">
-        <div class="col-12 col-md-9">
-          <p class="q-mt-none q-mb-xs text-weight-medium">Payment Address</p>
+      <div class="row q-mb-md q-col-gutter-md">
+        <div class="col-12 col-sm-6 col-lg-4">
+          <p class="q-mt-none q-mb-xs text-weight-medium">Email</p>
           <q-input
-            v-model="paymentAddress"
-            placeholder="Payment Address"
+            v-model="email"
+            placeholder="Email"
             outlined
             dense
             lazy-rules
             stack-label
+            :rules="[(val, rules) => !val || (val && rules.email(val)) || 'Please enter a valid email address']"
             hide-bottom-space
           >
           </q-input>
         </div>
+        <div class="col-12 col-sm-6 col-lg-4">
+          <p class="q-mt-none q-mb-xs text-weight-medium">Password</p>
+          <q-input
+            :type="isPwd ? 'password' : 'text'"
+            v-model="password"
+            outlined
+            dense
+            placeholder="Password"
+            lazy-rules
+            stack-label
+            :rules="[(val) => !val || val.length == 0 || (val && val.length >= 6) || 'Please use a valid password']"
+            hide-bottom-space
+          >
+            <template v-slot:append>
+              <q-icon
+                v-if="password && password.length > 0"
+                :name="isPwd ? 'visibility_off' : 'visibility'"
+                class="cursor-pointer"
+                @click="isPwd = !isPwd"
+              />
+            </template>
+          </q-input>
+        </div>
       </div>
-      <div class="row justify-end">
+      <div class="row">
+        <q-checkbox class="col-12 col-md-4" v-model="resetTOTP" label="Reset TOTP" />
+        <q-checkbox class="col-12 col-md-4" v-model="lockUser" label="Lock User (Disable Login)" />
+      </div>
+      <div class="row justify-end q-gutter-sm">
+        <q-btn label="Delete User" color="primary" @click="confirm = true" />
+        <q-dialog v-model="confirm" persistent>
+          <q-card>
+            <q-card-section class="row items-center">
+              <q-avatar icon="warning" color="primary" text-color="white" />
+              <span class="q-ml-sm">Are you sure to delete this user?</span>
+            </q-card-section>
+            <q-card-actions align="right">
+              <q-btn flat label="Delete" color="primary" v-close-popup @click="this.deleteUser()" />
+              <q-btn flat label="Cancel" color="primary" v-close-popup />
+            </q-card-actions>
+          </q-card>
+        </q-dialog>
         <q-btn label="Save" type="submit" color="primary" />
       </div>
     </q-form>
@@ -73,12 +100,14 @@ export default {
   name: 'pageUserDetails',
   data() {
     return {
-      paymentTypeOptions: PAYMENT_TYPE_OPTIONS,
       userName: '',
+      password: '',
       email: '',
       displayName: '',
-      paymentType: null,
-      paymentAddress: '',
+      isPwd: true,
+      resetTOTP: false,
+      lockUser: false,
+      confirm: false,
     }
   },
   computed: {},
@@ -90,25 +119,42 @@ export default {
       const id = this.$route.params.id
       this.$api.get(`/admin/user/info/${id}`).then((data) => {
         this.email = data.email
-        this.paymentAddress = data.paymentAddress
-        this.paymentType = data.paymenType
         this.userName = data.userName
         this.displayName = data.displayName
+        this.lockUser = data.locked
       })
     },
     updateUserInfo() {
       this.$api
-        .put(`user/info`, {
-          id: parseInt(this.$route.params.id),
-          email: this.email,
-          paymentAddress: this.paymentAddress,
-          paymentType: this.paymentType,
+        .put(`admin/user/info`, {
+          userId: parseInt(this.$route.params.id),
           userName: this.userName,
+          password: this.password,
+          email: this.email,
           displayName: this.displayName,
+          otp: this.resetTOTP,
+          locked: this.lockUser,
         })
         .then(() => {
           this.$q.notify({
             message: 'User saved',
+            color: 'positive',
+            icon: 'check',
+          })
+          this.$router.push({ name: 'user.list' })
+        })
+        .catch((err) => {
+          responseError(err)
+        })
+    },
+    deleteUser() {
+      this.$api
+        .put(`admin/user/delete`, {
+          userId: parseInt(this.$route.params.id),
+        })
+        .then(() => {
+          this.$q.notify({
+            message: 'User has been deleted successfully',
             color: 'positive',
             icon: 'check',
           })
