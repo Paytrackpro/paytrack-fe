5251721addffc4f71dadcf2c715ff793de070d4b
Justin Do <justin.namdo@gmail.com>
2023-05-09 21:24:12 +0700

    #127 Allow to save a draft with empty fields

--- src/components/payment/paymentForm.vue
+++ src/components/payment/paymentForm.vue
@@ -104,14 +104,14 @@
     </div>
     <div class="row justify-end q-gutter-sm">
       <q-btn
-        :label="inPayment.status === '' ? 'Mark as draft' : 'Update'"
+        :label="inPayment.status === '' ? 'Save as draft' : 'Update'"
         type="button"
         color="primary"
         @click="submit(true)"
         :disable="submitting"
       >
         <q-tooltip v-if="inPayment.status === ''">
-          'Mark as draft' will not notify the payment to the recipient
+          'Save as draft' will not notify the payment to the recipient
         </q-tooltip>
       </q-btn>
       <q-btn
@@ -218,19 +218,22 @@ export default {
       savePayment: 'payment/save',
     }),
     async submit(isDraft) {
-      if (this.submitting || !this.partner.contactMethod) {
+      if (this.submitting || (!isDraft && !this.partner.contactMethod)) {
         return
       }
       const inPutObject = await this.$refs.inputReceiver.validateAndGetValue()
-      const valid = await this.$refs.paymentForm.validate()
+      var valid
+      if (!isDraft) {
+        valid = await this.$refs.paymentForm.validate()
+      }
       const settingValid = await this.$refs.setting.$refs.selectedCoins.validate()
-      if (!valid || !settingValid) {
+      if ((!isDraft && !valid) || !settingValid) {
         return
       }
       if (this.inPayment.paymentSettings.length == 0) {
         return
       }
-      if (!this.inPayment.amount > 0) {
+      if (!isDraft && !this.inPayment.amount > 0) {
         this.$q.notify({
           type: 'negative',
           message:
@@ -240,18 +243,20 @@ export default {
       }
       const payment = { ...this.inPayment }
       payment.hourlyRate = Number(payment.hourlyRate)
-      payment.contactMethod = inPutObject.contactMethod
       if (isDraft !== true) {
         payment.status = 'sent'
       }
-      if (payment.contactMethod === 'email') {
-        if (!payment.id || this.user.id === payment.senderId) {
-          payment.externalEmail = inPutObject.value
+      if (!isDraft || (isDraft && inPutObject && inPutObject.value)) {
+        payment.contactMethod = inPutObject.contactMethod
+        if (payment.contactMethod === 'email') {
+          if (!payment.id || this.user.id === payment.senderId) {
+            payment.externalEmail = inPutObject.value
+          }
         }
+        payment.receiverId = inPutObject.id
       }
       payment.paymentMethod = this.setting.type
       payment.paymentAddress = this.setting.address
-      payment.receiverId = inPutObject.id
       payment.token = this.token
       this.submitting = true
       let successNotify = 'Payment request created'
