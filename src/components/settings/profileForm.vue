d1664a84cc24a80557e459fd37ec2536e31f6c18
justindo <justin.namdo@gmail.com>
2023-06-24 14:23:01 +0700

    fix monthly text

--- src/components/settings/profileForm.vue
+++ src/components/settings/profileForm.vue
@@ -33,7 +33,7 @@
         </div>
       </div>
     </div>
-    <q-checkbox label="Show Month Report" v-model="user.showMonthlyReport" />
+    <q-checkbox label="Show Monthly Report" v-model="user.showMonthlyReport" />
     <div class="q-mt-lg">
       <q-btn label="Save" class="q-mr-xs btn-animated btn" :disable="loading" type="submit" color="primary" />
     </div>
