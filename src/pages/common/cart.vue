26c8c4eadf6648b48cd000f014b78334d3e83c51
justindo <justin.namdo@gmail.com>
2023-10-27 01:04:28 +0700

    Create checkout page and handle actions related to cart and checkout

--- src/pages/common/cart.vue
+++ src/pages/common/cart.vue
@@ -1,72 +1,175 @@
 <template>
   <div class="q-pa-lg">
+    <q-btn flat icon="undo" type="button" color="primary" class="btn-animated btn q-mb-md btn-radius" @click="back">
+      <q-tooltip> {{ isCheckout ? 'Back to Cart Page' : 'Back to Homepage' }} </q-tooltip>
+    </q-btn>
     <q-card-section class="card-header q-pa-sm">
       <div class="row justify-between">
-        <div class="text-h6 title-case q-pt-sm q-mb-sm">Cart Page</div>
+        <div class="text-h6 title-case q-pt-sm q-mb-sm">{{ isCheckout ? 'Checkout Page' : 'Cart Page' }}</div>
         <div class="row align-center">
-          <p class="text-weight-medium text-primary">Total:</p>
+          <p class="text-weight-medium text-primary">{{ isCheckout ? 'Total Payment:' : 'Total:' }}</p>
           <p class="text-weight-medium text-primary" v-for="(currency, i) in currencyChoose" :key="i">
             <span class="q-ml-sm" v-if="i > 0">|</span>
             &nbsp;{{ totalPriceDisplay(currency) }}
           </p>
-          <q-btn label="Checkout" type="button" color="primary" class="q-mx-sm btn btn-animated" />
+          <q-btn
+            label="Checkout"
+            type="button"
+            color="primary"
+            class="q-mx-sm btn btn-animated"
+            @click="goCheckout()"
+            v-if="!isCheckout"
+          />
+          <q-btn
+            v-if="isCheckout"
+            label="Create Bill"
+            type="button"
+            color="primary"
+            class="q-mx-sm btn btn-animated"
+            :disable="phoneNumber == '' || address == ''"
+            @click="createBill()"
+          >
+            <q-tooltip v-if="phoneNumber == '' || address == ''"
+              >Phone number and address cannot be left blank</q-tooltip
+            >
+          </q-btn>
         </div>
       </div>
     </q-card-section>
-    <div class="row q-col-gutter-sm cart-products-area">
-      <div class="col-12 q-pa-sm" v-for="(ownerId, index) in ownerIds" :key="index">
-        <div class="row shadow-primary">
-          <div class="col-12 cart-owner-header q-pa-sm">
-            <q-checkbox v-model="ownerCheckboxs[index]" size="sm" @click="ownerCheckAll(ownerId, index)" />
-            <q-icon name="account_circle" class="q-ml-sm" color="grey-4" size="sm" />
-            <span class="q-ml-xs text-size-15 text-grey-3">{{ getOwnerName(cartData[ownerId]) }}</span>
+    <div class="row q-col-gutter-sm cart-products-area" v-if="isCheckout">
+      <div class="col-12 q-pa-sm">
+        <div class="shadow-primary q-pa-sm">
+          <div class="row q-mx-sm q-mt-sm">
+            <q-icon name="location_on" size="sm" class="q-mr-sm"></q-icon>
+            <p class="text-weight-medium text-size-15">Delivery Address</p>
           </div>
-          <div class="col-12" v-for="(cart, cartIndex) in cartData[ownerId]" :key="cartIndex">
-            <div class="row q-my-sm q-pa-sm cart-row">
+          <div class="row q-mx-sm q-py-sm align-center">
+            <q-chip class="glossy q-my-sm" square color="teal" text-color="white" icon="person">
+              {{ getDisplayName }}
+            </q-chip>
+            <q-input
+              class="q-ml-sm q-my-sm"
+              v-model="phoneNumber"
+              outlined
+              dense
+              lazy-rules
+              stack-label
+              placeholder="Phone Number"
+            >
+            </q-input>
+            <q-input
+              class="q-ml-sm col-12 col-sm-6 col-lg-4 q-my-sm"
+              v-model="address"
+              outlined
+              dense
+              lazy-rules
+              stack-label
+              placeholder="Address"
+            >
+            </q-input>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row q-col-gutter-sm cart-products-area">
+      <div v-for="(ownerId, index) in ownerIds" :key="index" class="owner-card">
+        <div class="col-12 q-pa-sm shadow-primary" v-if="ownerDisplay(index)">
+          <div class="row">
+            <div class="col-12 q-pa-sm">
               <q-checkbox
-                v-model="productCheckbox[ownerId][cartIndex]"
+                v-model="ownerCheckboxs[index]"
                 size="sm"
-                @click="oneProductChecked(ownerId, index)"
+                @click="ownerCheckAll(ownerId, index)"
+                v-if="!isCheckout"
               />
-              <q-img
-                :src="getAvatarSrc(cart.avatarBase64)"
-                style="width: 100%; max-width: 70px"
-                class="q-ml-md col-4 col-lg-1 col-sm-1 col-md-3 col-xs-6"
-              ></q-img>
-              <p class="q-ml-lg col-8 col-lg-4 col-sm-3 col-md-4 product-title">{{ cart.productName }}</p>
-              <p class="q-ml-lg col-3 col-sm-1 col-md-1">{{ priceDisplay(cart.price, cart.currency) }}</p>
-              <div class="col-2 col-sm-2 col-md-2">
-                <q-input
-                  color="primary"
-                  class="q-mr-sm"
-                  outlined
-                  lazy-rules
-                  v-model="cart.quantity"
-                  auto
-                  type="number"
-                  style="max-width: 150px"
-                  dense
-                  v-on:blur="updateCart(cart, index, cartIndex)"
-                  @focus="saveBeforeQuantity(cart)"
-                  :rules="[
-                    (val) =>
-                      val <= cart.stock || 'Order quantity cannot be greater than stock (' + cart.stock + ' products)',
-                  ]"
-                />
+              <q-icon name="account_circle" class="q-ml-sm" color="grey-4" size="sm" />
+              <span class="q-ml-xs text-size-15 text-grey-3">{{ getOwnerName(cartData[ownerId]) }}</span>
+              <q-separator />
+            </div>
+            <div class="col-12">
+              <div v-for="(cart, cartIndex) in cartData[ownerId]" :key="cartIndex">
+                <div class="row q-my-sm q-pa-sm cart-row" v-if="productDipslay(ownerId, cartIndex)">
+                  <q-checkbox
+                    v-model="productCheckbox[ownerId][cartIndex]"
+                    size="sm"
+                    @click="oneProductChecked(ownerId, index)"
+                    v-if="!isCheckout"
+                  />
+                  <q-img
+                    :src="getAvatarSrc(cart.avatarBase64)"
+                    style="width: 100%; max-width: 70px"
+                    class="q-ml-md col-4 col-lg-1 col-sm-1 col-md-3 col-xs-6"
+                  ></q-img>
+                  <p class="q-ml-lg col-8 col-lg-4 col-sm-3 col-md-4 product-title">{{ cart.productName }}</p>
+                  <p class="q-ml-lg col-3 col-sm-1 col-md-1">{{ priceDisplay(cart.price, cart.currency) }}</p>
+                  <div class="col-2 col-sm-2 col-md-2" v-if="!isCheckout">
+                    <q-input
+                      color="primary"
+                      class="q-mr-sm"
+                      outlined
+                      lazy-rules
+                      v-model="cart.quantity"
+                      auto
+                      type="number"
+                      style="max-width: 150px"
+                      dense
+                      v-on:blur="updateCart(cart, index, cartIndex)"
+                      @focus="saveBeforeQuantity(cart)"
+                      :rules="[
+                        (val) =>
+                          val <= cart.stock ||
+                          'Order quantity cannot be greater than stock (' + cart.stock + ' products)',
+                      ]"
+                    />
+                  </div>
+                  <p class="col-2 col-sm-2 col-md-2" v-if="isCheckout">{{ cart.quantity }}</p>
+                  <p class="q-ml-lg col-2 col-sm-1 col-md-1 text-accent">
+                    {{ priceDisplay(cart.price * cart.quantity, cart.currency) }}
+                  </p>
+                  <q-btn
+                    class="col-1 col-md-1 col-sm-1"
+                    dense
+                    flat
+                    color="accent"
+                    icon="delete"
+                    v-if="!isCheckout"
+                    @click="setDeleteCart(cart)"
+                  >
+                    <q-tooltip> Delete Product </q-tooltip>
+                  </q-btn>
+                </div>
               </div>
-              <p class="q-ml-lg col-2 col-sm-1 col-md-1 text-accent">
-                {{ priceDisplay(cart.price * cart.quantity, cart.currency) }}
-              </p>
-              <q-btn
-                class="col-1 col-md-1 col-sm-1"
-                dense
-                flat
-                color="accent"
-                icon="delete"
-                @click="setDeleteCart(cart)"
-              >
-                <q-tooltip> Delete Product </q-tooltip>
-              </q-btn>
+            </div>
+          </div>
+          <q-separator v-if="isCheckout" />
+          <div class="row justify-between" v-if="isCheckout">
+            <div class="col-8 q-pa-sm">
+              <div class="row align-center">
+                <p class="text-weight-medium">Note</p>
+                <q-input
+                  class="q-ml-sm q-my-sm col-6"
+                  v-model="ownerNotes[ownerId]"
+                  outlined
+                  dense
+                  lazy-rules
+                  stack-label
+                  placeholder="Note for seller"
+                >
+                </q-input>
+              </div>
+            </div>
+            <div class="q-pa-sm">
+              <p class="text-size-15 text-weight-medium">Total Amount</p>
+              <div class="row">
+                <p
+                  class="text-weight-medium text-primary"
+                  v-for="(ownerCurrency, curIndex) in ownerCurrencyChoose[ownerId]"
+                  :key="curIndex"
+                >
+                  <span class="q-ml-sm" v-if="curIndex > 0">|</span>
+                  &nbsp;{{ ownerAmount(ownerCurrency, ownerId) }}
+                </p>
+              </div>
             </div>
           </div>
         </div>
@@ -105,6 +208,7 @@ export default {
     return {
       ownerIds: [],
       ownerCheckboxs: [],
+      ownerNotes: {},
       cartData: {},
       productCheckbox: {},
       beforeQuantity: 0,
@@ -113,6 +217,10 @@ export default {
       deleteProductId: 0,
       selectedProductIds: [],
       currencyChoose: [],
+      ownerCurrencyChoose: {},
+      isCheckout: false,
+      phoneNumber: '',
+      address: '',
     }
   },
   created() {
@@ -249,6 +357,97 @@ export default {
       })
       return this.priceDisplay(sum, currency)
     },
+    goCheckout() {
+      this.ownerIds.forEach((id, i) => {
+        this.updateOwnerCurrency(id)
+        this.ownerNotes[id] = ''
+      })
+      this.isCheckout = true
+    },
+    productDipslay(ownerId, cartIndex) {
+      if (!this.isCheckout) {
+        return true
+      }
+      return this.productCheckbox[ownerId][cartIndex]
+    },
+    ownerDisplay(ownerIndex) {
+      if (!this.isCheckout || this.ownerCheckboxs[ownerIndex]) {
+        return true
+      }
+      let result = false
+      this.productCheckbox[this.ownerIds[ownerIndex]].forEach((v, i) => {
+        if (v) {
+          result = true
+        }
+      })
+      return result
+    },
+    ownerAmount(ownerCurrency, ownerId) {
+      let sum = 0
+      this.productCheckbox[ownerId].forEach((v, i) => {
+        if (v && this.cartData[ownerId][i].currency == ownerCurrency) {
+          sum += this.cartData[ownerId][i].price * this.cartData[ownerId][i].quantity
+        }
+      })
+      return this.priceDisplay(sum, ownerCurrency)
+    },
+    back() {
+      if (this.isCheckout) {
+        this.isCheckout = false
+        return
+      }
+      this.$router.push({ path: `/home` })
+    },
+    updateOwnerCurrency(ownerId) {
+      let ownerCurrencies = []
+      this.productCheckbox[ownerId].forEach((v, i) => {
+        if (v && !ownerCurrencies.includes(this.cartData[ownerId][i].currency)) {
+          ownerCurrencies.push(this.cartData[ownerId][i].currency)
+        }
+      })
+      this.ownerCurrencyChoose[ownerId] = ownerCurrencies
+    },
+    createBill() {
+      let prepareData = []
+      this.ownerIds.forEach((ownerId, index) => {
+        let ownerProductData = []
+        this.productCheckbox[ownerId].forEach((v, i) => {
+          if (v) {
+            let cart = this.cartData[ownerId][i]
+            ownerProductData.push({
+              ProductId: cart.productId,
+              Price: cart.price,
+              Quantity: cart.quantity,
+              Currency: cart.currency,
+              Amount: cart.price * cart.quantity,
+            })
+          }
+        })
+        if (ownerProductData.length > 0) {
+          prepareData.push({
+            OwnerId: ownerId,
+            ProductPayments: ownerProductData,
+            PhoneNumber: this.phoneNumber,
+            Address: this.address,
+            Memo: this.ownerNotes[ownerId],
+          })
+        }
+      })
+
+      this.$api
+        .post('order/createOrders', { OrderData: prepareData })
+        .then((data) => {
+          this.$q.notify({
+            message: 'Order Create Successfully',
+            color: 'positive',
+            icon: 'check',
+          })
+        })
+        .catch((err) => {
+          responseError(err)
+          return { error: err }
+        })
+    },
   },
   computed: {
     ...mapGetters({
@@ -264,6 +463,12 @@ export default {
         })
       })
       return sum
+    },
+    getDisplayName() {
+      if (this.user.displayName == '') {
+        return this.user.userName
+      }
+      return this.user.displayName
     },
   },
 }
