d587e7cc16810eba431721c1c8ec802032a2c3e6
justindo <justin.namdo@gmail.com>
2023-11-26 18:11:01 +0700

    Add and fix report feature

---
+++ src/components/report/reportFilter.vue
@@ -0,0 +1,356 @@
+<template>
+  <div class="row q-mb-md q-col-gutter-md">
+    <div class="col-12 col-sm-4 col-md-3 q-py-sm q-my-sm field-shadow">
+      <p class="q-mt-none q-mb-xs text-weight-medium">Date Range</p>
+      <q-field outlined stack-label class="date-range-field">
+        <span>{{ `${paymentDateRange.from} - ${paymentDateRange.to}` }}</span>
+        <q-popup-proxy transition-show="scale" transition-hide="scale">
+          <q-list style="min-width: 100px" class="cursor-pointer">
+            <q-item clickable v-close-popup @click="setRange(0)">
+              <q-item-section>Last 24h </q-item-section>
+            </q-item>
+            <q-item clickable v-close-popup @click="setRange(1)">
+              <q-item-section>This month </q-item-section>
+            </q-item>
+            <q-item clickable v-close-popup @click="setRange(2)">
+              <q-item-section>Last month </q-item-section>
+            </q-item>
+            <q-item clickable v-close-popup @click="setRange(3)">
+              <q-item-section>Current Year </q-item-section>
+            </q-item>
+            <q-item clickable v-close-popup @click="setRange(4)">
+              <q-item-section>Last Year </q-item-section>
+            </q-item>
+            <q-item clickable v-close-popup @click="setRange(5)">
+              <q-item-section>All</q-item-section>
+            </q-item>
+            <q-item clickable v-close-popup @click="customRangeChoose()">
+              <q-item-section>Custom Range</q-item-section>
+            </q-item>
+          </q-list>
+        </q-popup-proxy>
+      </q-field>
+    </div>
+    <div class="col-12 col-sm-4 col-md-3 q-py-sm q-my-sm field-shadow">
+      <p class="q-mt-none q-mb-xs text-weight-medium">Members</p>
+      <q-select
+        v-model="memberModel"
+        use-input
+        use-chips
+        multiple
+        outlined
+        input-debounce="0"
+        @new-value="createMemberValue"
+        :options="memberFilterOptions"
+        @filter="filterMemberFn"
+      />
+    </div>
+    <div class="col-12 col-sm-4 col-md-3 q-py-sm q-my-sm field-shadow">
+      <p class="q-mt-none q-mb-xs text-weight-medium">Projects</p>
+      <q-select
+        v-model="projectModel"
+        use-input
+        use-chips
+        multiple
+        outlined
+        input-debounce="0"
+        @new-value="createProjectValue"
+        :options="projectFilterOptions"
+        @filter="filterProjectFn"
+      />
+    </div>
+  </div>
+  <q-dialog v-model="customRangeDialog">
+    <q-card>
+      <q-date v-model="customRange" range>
+        <div class="row items-center justify-end">
+          <q-btn v-close-popup label="OK" color="primary" flat /></div
+      ></q-date>
+    </q-card>
+  </q-dialog>
+</template>
+<script>
+import { ref } from 'vue'
+let memberStringOptions = []
+let projectStringOptions = []
+export default {
+  name: 'reportFilter',
+  props: {
+    tabChange: Boolean,
+  },
+  setup() {
+    const memberModel = ref(null)
+    const memberFilterOptions = ref(memberStringOptions)
+    const projectModel = ref(null)
+    const projectFilterOptions = ref(projectStringOptions)
+
+    return {
+      memberModel,
+      memberFilterOptions,
+      projectModel,
+      projectFilterOptions,
+
+      createMemberValue(val, done) {
+        if (val.length > 0) {
+          const modelValue = (memberModel.value || []).slice()
+
+          val
+            .split(/[,;|]+/)
+            .map((v) => v.trim())
+            .filter((v) => v.length > 0)
+            .forEach((v) => {
+              if (memberStringOptions.includes(v) === false) {
+                memberStringOptions.push(v)
+              }
+              if (modelValue.includes(v) === false) {
+                modelValue.push(v)
+              }
+            })
+          done(null)
+          memberModel = ref(modelValue)
+        }
+      },
+      createProjectValue(val, done) {
+        if (val.length > 0) {
+          const modelValue = (projectModel.value || []).slice()
+
+          val
+            .split(/[,;|]+/)
+            .map((v) => v.trim())
+            .filter((v) => v.length > 0)
+            .forEach((v) => {
+              if (projectStringOptions.includes(v) === false) {
+                projectStringOptions.push(v)
+              }
+              if (modelValue.includes(v) === false) {
+                modelValue.push(v)
+              }
+            })
+          done(null)
+          projectModel.value = modelValue
+        }
+      },
+
+      filterMemberFn(val, update) {
+        update(() => {
+          if (val === '') {
+            memberFilterOptions.value = memberStringOptions
+          } else {
+            const needle = val.toLowerCase()
+            memberFilterOptions.value = memberStringOptions.filter((v) => v.toLowerCase().indexOf(needle) > -1)
+          }
+        })
+      },
+      filterProjectFn(val, update) {
+        update(() => {
+          if (val === '') {
+            projectFilterOptions.value = projectStringOptions
+          } else {
+            const needle = val.toLowerCase()
+            projectFilterOptions.value = projectStringOptions.filter((v) => v.toLowerCase().indexOf(needle) > -1)
+          }
+        })
+      },
+    }
+  },
+  created() {
+    this.initUserList()
+    this.initProjectList()
+    this.paymentDateRange = ref({ from: this.getRefDateFormat(new Date(+0)), to: this.getRefDateFormat(new Date()) })
+  },
+  data() {
+    return {
+      paymentDateRange: ref({ from: '2022/01/01', to: '2022/01/05' }),
+      userSelection: [],
+      projectSelection: [],
+      reportFilters: {
+        startDate: new Date(+0),
+        endDate: new Date(),
+        memberIds: [],
+        projectIds: [],
+      },
+      customRangeDialog: false,
+      customRange: ref({
+        from: this.get7DaysBefore(),
+        to: this.getRefDateFormat(new Date()),
+      }),
+      isCustomRange: false,
+    }
+  },
+  methods: {
+    initUserList() {
+      this.$api
+        .get('/user/get-user-list', {})
+        .then((res) => {
+          this.userSelection = res
+          memberStringOptions = []
+          this.userSelection.forEach((userInfo) => {
+            memberStringOptions.push(userInfo.userName)
+          })
+        })
+        .catch((err) => {
+          responseError(err)
+        })
+    },
+    initProjectList() {
+      this.$api
+        .get('/project/get-list', {})
+        .then((res) => {
+          this.projectSelection = res
+          projectStringOptions = []
+          this.projectSelection.forEach((project) => {
+            projectStringOptions.push(project.projectName)
+          })
+        })
+        .catch((err) => {
+          responseError(err)
+        })
+    },
+    setRange(code) {
+      let start = new Date()
+      let end = new Date()
+      let tmpDate
+      this.isCustomRange = false
+      switch (code) {
+        case 0:
+          start.setDate(start.getDate() - 1)
+          break
+        case 1:
+          start = new Date(start.getFullYear(), start.getMonth(), 1)
+          break
+        case 2:
+          tmpDate = new Date()
+          tmpDate.setMonth(tmpDate.getMonth() - 1)
+          start = new Date(tmpDate.getFullYear(), tmpDate.getMonth(), 1)
+          end = new Date(tmpDate.getFullYear(), tmpDate.getMonth() + 1, 0)
+          break
+        case 3:
+          start = new Date(start.getFullYear(), 0, 1)
+          break
+        //last year
+        case 4:
+          tmpDate = new Date()
+          tmpDate.setFullYear(tmpDate.getFullYear() - 1)
+          start = new Date(tmpDate.getFullYear(), 0, 1)
+          end = new Date(tmpDate.getFullYear(), 11, 31)
+          break
+        case 5:
+          start = new Date(+0)
+          break
+      }
+
+      this.reportFilters.startDate = start
+      this.reportFilters.endDate = end
+      this.paymentDateRange = ref({ from: this.getRefDateFormat(start), to: this.getRefDateFormat(end) })
+      this.$emit('updateFilter', this.reportFilters)
+    },
+    getRefDateFormat(date) {
+      return ref(
+        date.getFullYear() +
+          '/' +
+          (date.getMonth() < 9 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) +
+          '/' +
+          date.getDate()
+      )
+    },
+    getUserInfo(userName) {
+      let result = null
+      this.userSelection.forEach((userInfo) => {
+        if (userInfo.userName == userName) {
+          result = userInfo
+          return
+        }
+      })
+      return result
+    },
+    getProjectInfo(projectName) {
+      let result = null
+      this.projectSelection.forEach((projectInfo) => {
+        if (projectInfo.projectName == projectName) {
+          result = projectInfo
+          return
+        }
+      })
+      return result
+    },
+    customRangeChoose() {
+      ;``
+      this.customRangeDialog = true
+      this.isCustomRange = true
+    },
+    get7DaysBefore() {
+      const now = new Date()
+      now.setDate(now.getDate() - 6)
+      return this.getRefDateFormat(now)
+    },
+  },
+  watch: {
+    memberModel: {
+      immediate: true,
+      handler(newVal) {
+        let memberIds = []
+        if (newVal == null) {
+          return
+        }
+        let newSelectedUsers = String(newVal)
+        newSelectedUsers
+          .split(/[,;|]+/)
+          .map((v) => v.trim())
+          .filter((v) => v.length > 0)
+          .forEach((v) => {
+            let userInfo = this.getUserInfo(v)
+            if (userInfo == null) {
+              return
+            }
+            memberIds.push(userInfo.id)
+          })
+        this.reportFilters.memberIds = memberIds
+        this.$emit('updateFilter', this.reportFilters)
+      },
+    },
+    projectModel: {
+      immediate: true,
+      handler(newVal) {
+        let projectIds = []
+        if (newVal == null) {
+          return
+        }
+        let newSelectedProjects = String(newVal)
+        newSelectedProjects
+          .split(/[,;|]+/)
+          .map((v) => v.trim())
+          .filter((v) => v.length > 0)
+          .forEach((v) => {
+            let projectInfo = this.getProjectInfo(v)
+            if (projectInfo == null) {
+              return
+            }
+            projectIds.push(projectInfo.projectId)
+          })
+        this.reportFilters.projectIds = projectIds
+        this.$emit('updateFilter', this.reportFilters)
+      },
+    },
+    customRange: {
+      immediate: true,
+      handler(newVal) {
+        if (!this.isCustomRange || !newVal) {
+          return
+        }
+        const startDate = new Date(newVal.from)
+        const endDate = new Date(newVal.to)
+        this.reportFilters.startDate = startDate
+        this.reportFilters.endDate = endDate
+        this.paymentDateRange = ref({ from: this.getRefDateFormat(startDate), to: this.getRefDateFormat(endDate) })
+        this.$emit('updateFilter', this.reportFilters)
+      },
+    },
+  },
+}
+</script>
+
+<style scoped>
+.invoice-price-detail {
+  max-height: 150px;
+}
+</style>
--- src/pages/admin/report.vue
+++ src/pages/admin/report.vue
@@ -1,6 +1,14 @@
 <template>
   <q-card class="q-pa-md q-ma-lg content-container">
-    <q-card-section class="q-py-none">
+    <q-card-section class="card-header">
+      <div class="row justify-between">
+        <div class="row">
+          <div class="text-h6 title-case">Summary Report</div>
+        </div>
+      </div>
+    </q-card-section>
+    <q-card-section class="q-pt-md">
+      <report-filter @updateFilter="updateFilter" :tabChange="tabChange"></report-filter>
       <q-tabs align="left" v-model="tab" inline-label active-class="tab-active">
         <q-tab label="Payment Report" icon="analytics" name="payment" no-caps class="tab-item" />
         <q-tab label="Invoice Summary" icon="summarize" name="invoice" no-caps class="tab-item" />
@@ -21,90 +29,146 @@
         transition-next="jump-down"
       >
         <q-tab-panel class="q-pa-none" name="payment">
-          <div class="row q-mb-md q-col-gutter-md">
-            <div class="col-12 col-sm-4 col-md-3 q-py-sm q-my-sm field-shadow">
-              <q-field outlined>
-                <span>{{ `${paymentDateRange.from} - ${paymentDateRange.to}` }}</span>
-                <q-popup-proxy transition-show="scale" transition-hide="scale">
-                  <q-date v-model="paymentDateRange" range>
-                    <div class="row items-center justify-end">
-                      <q-btn v-close-popup label="Close" color="primary" flat />
-                    </div>
-                  </q-date>
-                </q-popup-proxy>
-              </q-field>
+          <div class="row">
+            <div class="col-12">
+              <q-card class="no-shadow outlined">
+                <div class="row text-weight-medium report-header-table">
+                  <span class="col-4 text-center">Month</span>
+                  <span class="col-4 text-center">Amout(USD)</span>
+                </div>
+              </q-card>
+              <q-list bordered class="rounded-borders">
+                <q-expansion-item
+                  v-model="paymentReportExpansion[i]"
+                  expand-separator
+                  icon="perm_identity"
+                  class="report-row-header"
+                  v-for="(paymentReport, i) of paymentReportList"
+                  :key="i"
+                >
+                  <template v-slot:header>
+                    <q-item-section>
+                      <div class="row">
+                        <span class="col-4 text-center">{{ paymentReport.month }}</span>
+                        <span class="col-4 text-center padding-30"
+                          >$&nbsp;{{ getPaymentReportAmount(paymentReport) }}</span
+                        >
+                      </div>
+                    </q-item-section>
+                  </template>
+                  <q-separator />
+                  <q-card>
+                    <q-card-section class="no-pm-top">
+                      <div class="row q-mt-lg" v-for="(paymentUnit, j) of paymentReport.paymentUnits" :key="j">
+                        <span class="col-4 text-center">{{ paymentUnit.displayName }}</span>
+                        <span class="col-4 text-center">$&nbsp;{{ paymentUnit.amount }}</span>
+                        <span class="col-4 text-center" v-if="paymentUnit.paymentMethod != ''">{{
+                          paymentUnit.expectedAmount + ' (' + paymentUnit.paymentMethod.toUpperCase() + ')'
+                        }}</span>
+                      </div>
+                    </q-card-section>
+                  </q-card>
+                </q-expansion-item>
+              </q-list>
             </div>
           </div>
         </q-tab-panel>
         <q-tab-panel class="q-pa-none" name="invoice">
-          <p class="text-h6">Summary</p>
-          <div class="row q-mb-md q-col-gutter-md">
-            <div class="col-12 col-sm-6 col-lg-4 q-py-sm q-my-sm field-shadow">
-              <p class="text-bold">Select Month</p>
-              <q-select
-                v-model="selectedMonth"
-                :options="monthChoose"
-                outlined
-                dense
-                style="max-width: 250px"
-                lazy-rules
-                stack-label
-                emit-value
-                map-options
-                borderless
-              />
-            </div>
-            <div class="col-12 col-sm-6 col-lg-4 q-py-sm q-my-sm field-shadow">
-              <p class="text-bold">Payees filter</p>
-              <q-input
-                outlined
-                dense
-                lazy-rules
-                stack-label
-                @blur="checkValidPayees"
-                :error="payeesError"
-                :error-message="payees.error"
-                hide-bottom-space
-                v-model="payees.value"
-                placeholder="Separate with comma(,)"
-              >
-                <template v-slot:append>
-                  <q-icon v-if="payees.value !== ''" name="close" @click="payees.value = ''" class="cursor-pointer" />
-                </template>
-              </q-input>
-              <q-chip
-                v-for="(item, index) in payees.usernames"
-                :key="item"
-                :clickable="false"
-                square
-                dense
-                text-color="white"
-                :color="index % 2 ? 'positive' : 'warning'"
-                icon="person"
-              >
-                {{ item }}
-              </q-chip>
-            </div>
-          </div>
-          <div class="row q-mb-md q-col-gutter-md">
-            <p class="text-bold">Start From: 01/{{ selectedMonth }}/2023</p>
-          </div>
-          <div class="row q-mb-md q-col-gutter-md">
-            <div class="col-12 col-sm-6 col-lg-4 q-py-sm q-my-sm field-shadow">
-              <p>
-                <span class="text-bold">Total Requests Received:</span> {{ paymentSummary.requestReceived }} requests
-              </p>
-              <p><span class="text-bold">Total Requests Sent: </span>{{ paymentSummary.requestSent }} requests</p>
-              <p><span class="text-bold">Total Requests Paid: </span>{{ paymentSummary.requestPaid }} requests</p>
-            </div>
-            <div class="col-12 col-sm-6 col-lg-4 q-py-sm q-my-sm field-shadow">
-              <p><span class="text-bold">Total Paid: </span>${{ paymentSummary.totalPaid }}</p>
-              <p><span class="text-bold">Total Received: </span>${{ paymentSummary.totalReceived }}</p>
+          <div class="row">
+            <div class="col-12">
+              <q-card class="no-shadow outlined">
+                <div class="row text-weight-medium report-header-table">
+                  <span class="col-4 text-center">Project</span>
+                  <span class="col-4 text-center">User</span>
+                  <span class="col-4 text-center">Total Hours</span>
+                </div>
+              </q-card>
+              <q-list bordered class="rounded-borders">
+                <q-expansion-item
+                  v-model="invoiceReportExpansion[i]"
+                  expand-separator
+                  class="report-row-header"
+                  v-for="(invoiceKey, i) of invoiceReportKey"
+                  :key="i"
+                >
+                  <template v-slot:header>
+                    <q-item-section>
+                      <div class="row">
+                        <span class="col-4 text-center">{{
+                          invoiceKey.split(';')[1] == '' ? '#' : invoiceKey.split(';')[1]
+                        }}</span>
+                        <span class="col-4 text-center padding-30">{{ invoiceKey.split(';')[0] }}</span>
+                        <span class="col-4 text-center padding-80">{{ getTotalHours(invoiceKey) }} hour(s)</span>
+                      </div>
+                    </q-item-section>
+                  </template>
+                  <q-separator />
+                  <q-card>
+                    <q-card-section class="no-pm-top">
+                      <div class="row q-mt-lg" v-for="(invoiceUnit, j) of invoiceReportList[invoiceKey]" :key="j">
+                        <span class="col-4 text-center">{{ invoiceUnit.date }}</span>
+                        <span class="col-4 text-center">{{ invoiceUnit.description }}</span>
+                        <span class="col-4 text-center">{{ invoiceUnit.hours }}&nbsp;hour(s)</span>
+                      </div>
+                    </q-card-section>
+                  </q-card>
+                </q-expansion-item>
+              </q-list>
             </div>
           </div>
         </q-tab-panel>
         <q-tab-panel class="q-pa-none" name="address">
-          <payment-list ref="paymentList" type="approval" label="Pending Approvals" />
+          <div class="row">
+            <div class="col-12">
+              <q-card class="no-shadow outlined">
+                <div class="row text-weight-medium report-header-table">
+                  <span class="col-4 text-center">Address</span>
+                  <span class="col-4 text-center">Payment Method</span>
+                  <span class="col-4 text-center">Display Name</span>
+                </div>
+              </q-card>
+              <q-list bordered class="rounded-borders">
+                <q-expansion-item
+                  v-model="addressReportExpansion[i]"
+                  expand-separator
+                  icon="perm_identity"
+                  v-for="(address, i) of addressReportKey"
+                  :key="i"
+                  class="report-row-header"
+                >
+                  <template v-slot:header>
+                    <q-item-section>
+                      <div class="row">
+                        <span class="col-4 text-center">{{ address }}</span>
+                        <span class="col-4 text-center padding-30">{{
+                          addressReportList[address].paymentMethod.toUpperCase()
+                        }}</span>
+                        <span class="col-4 text-center padding-80">{{ addressReportList[address].displayName }}</span>
+                      </div>
+                    </q-item-section>
+                  </template>
+                  <q-separator />
+                  <q-card>
+                    <q-card-section class="no-pm-top">
+                      <div
+                        class="row q-mt-lg"
+                        v-for="(addressUnit, j) of addressReportList[address].addressUnits"
+                        :key="j"
+                      >
+                        <span class="col-4 text-center">{{ addressUnit.dateTime }}</span>
+                        <span class="col-4 text-center"
+                          >{{ addressUnit.expectedAmount }}&nbsp;{{
+                            addressReportList[address].paymentMethod.toUpperCase()
+                          }}</span
+                        >
+                        <span class="col-4 text-center">$&nbsp;{{ addressUnit.amount }}</span>
+                      </div>
+                    </q-card-section>
+                  </q-card>
+                </q-expansion-item>
+              </q-list>
+            </div>
+          </div>
         </q-tab-panel>
       </q-tab-panels>
     </q-card-section>
@@ -113,24 +177,22 @@
 
 <script>
 import { mapGetters } from 'vuex'
-import { ref } from 'vue'
-import { date } from 'quasar'
-import { PaymentList } from 'components/payment'
-import customField from 'src/components/common/custom_field.vue'
+import reportFilter from 'components/report/reportFilter'
 import {
   DESTINATION_CHECK_NONE,
   DESTINATION_CHECK_DONE,
   DESTINATION_CHECK_CHECKING,
   DESTINATION_CHECK_FAIL,
 } from 'src/consts/common'
+
 export default {
   name: 'reportPage',
   components: {
-    PaymentList,
+    reportFilter,
   },
   data() {
     return {
-      tab: 'approval_list',
+      tab: 'payment',
       loading: false,
       monthChoose: [],
       selectedMonth: 1,
@@ -142,10 +204,50 @@ export default {
         status: DESTINATION_CHECK_NONE,
         error: '',
       },
-      paymentDateRange: ref({ from: '2022/01/01', to: '2022/01/05' }),
+      reportFilters: {
+        StartDate: null,
+        EndDate: null,
+        MemberIds: '',
+        ProjectIds: '',
+      },
+      dialogOpen: false,
+      expansionItem: false,
+      paymentReportList: [],
+      invoiceReportList: [],
+      addressReportList: [],
+      paymentReportExpansion: [],
+      addressReportExpansion: {},
+      invoiceReportExpansion: {},
+      invoiceReportKey: [],
+      addressReportKey: [],
+      tabChange: false,
+    }
+  },
+  created() {
+    this.reportFilters.StartDate = new Date(+0)
+    this.reportFilters.EndDate = new Date()
+    if (this.tab == 'payment') {
+      this.initPaymentReportList()
+    } else if (this.tab == 'invoice') {
+      this.initInvoiceReportList()
+    } else if (this.tab == 'address') {
+      this.initAddressReportList()
     }
   },
   methods: {
+    updateFilter(data) {
+      this.reportFilters.StartDate = data.startDate
+      this.reportFilters.EndDate = data.endDate
+      this.reportFilters.MemberIds = data.memberIds.length > 0 ? data.memberIds.join(',') : ''
+      this.reportFilters.ProjectIds = data.projectIds.length > 0 ? data.projectIds.join(',') : ''
+      if (this.tab == 'payment') {
+        this.initPaymentReportList()
+      } else if (this.tab == 'invoice') {
+        this.initInvoiceReportList()
+      } else if (this.tab == 'address') {
+        this.initAddressReportList()
+      }
+    },
     getPaymentSummary(month) {
       this.$api
         .get('/payment/monthly-summary', {
@@ -191,6 +293,78 @@ export default {
           this.payees.usernames = []
         })
     },
+    async initPaymentReportList() {
+      this.paymentReportExpansion = []
+      this.$api
+        .get('/payment/payment-report', {
+          params: this.reportFilters,
+        })
+        .then((res) => {
+          this.paymentReportList = res
+          res.forEach((item) => {
+            this.paymentReportExpansion.push(false)
+          })
+        })
+        .catch((err) => {
+          responseError(err)
+        })
+    },
+    async initInvoiceReportList() {
+      this.invoiceReportExpansion = []
+      this.invoiceReportKey = []
+      this.invoiceReportList = {}
+      this.$api
+        .get('/payment/invoice-report', {
+          params: this.reportFilters,
+        })
+        .then((res) => {
+          this.invoiceReportList = res
+          Object.keys(res).forEach((item) => {
+            this.invoiceReportExpansion.push(false)
+            this.invoiceReportKey.push(item)
+          })
+        })
+        .catch((err) => {
+          responseError(err)
+        })
+    },
+    async initAddressReportList() {
+      this.addressReportList = {}
+      this.addressReportKey = []
+      this.addressReportExpansion = []
+      this.$api
+        .get('/payment/address-report', {
+          params: this.reportFilters,
+        })
+        .then((res) => {
+          this.addressReportList = res
+          Object.keys(res).forEach((item) => {
+            this.addressReportExpansion.push(false)
+            this.addressReportKey.push(item)
+          })
+        })
+        .catch((err) => {
+          responseError(err)
+        })
+    },
+    getPaymentReportAmount(paymentReport) {
+      if (!paymentReport.paymentUnits || paymentReport.paymentUnits.length < 1) {
+        return ''
+      }
+      let totalCost = 0
+      paymentReport.paymentUnits.forEach((unit) => {
+        totalCost += unit.amount
+      })
+      return totalCost
+    },
+    getTotalHours(invoiceKey) {
+      const invoiceReport = this.invoiceReportList[invoiceKey]
+      let totalHours = 0
+      invoiceReport.forEach((item) => {
+        totalHours += item.hours
+      })
+      return totalHours
+    },
   },
   beforeMount() {
     const nowMonth = new Date().getMonth() + 1
@@ -213,6 +387,28 @@ export default {
       immediate: true,
       handler(newMonth) {
         this.getPaymentSummary(newMonth)
+      },
+    },
+    tab: {
+      immediate: true,
+      handler(newTab) {
+        if (this.reportFilters.StartDate == null && this.reportFilters.EndDate == null) {
+          return
+        }
+        this.reportFilter = {
+          StartDate: new Date(+0),
+          EndDate: new Date(),
+          MemberIds: '',
+          ProjectIds: '',
+        }
+        this.tabChange = !this.tabChange
+        if (newTab == 'payment') {
+          this.initPaymentReportList()
+        } else if (newTab == 'invoice') {
+          this.initInvoiceReportList()
+        } else if (newTab == 'address') {
+          this.initAddressReportList()
+        }
       },
     },
   },
