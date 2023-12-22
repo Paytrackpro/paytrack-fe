4f1ddb2b9d282f3201146ee54d66501da16bb57e
justindo <justin.namdo@gmail.com>
2023-12-23 03:56:14 +0700

    Remove background color on dialog item when mouse hover

--- src/components/payment/paymentList.vue
+++ src/components/payment/paymentList.vue
@@ -90,7 +90,7 @@
               :style="'height: ' + getScrollHeight() + 'px; max-width: ' + getDialogWidth() + 'px'"
             >
               <q-list>
-                <q-item v-for="item in selected" :key="item.id" clickable v-ripple class="q-mt-md bg-grey-8 rounded">
+                <q-item v-for="item in selected" :key="item.id" v-ripple class="q-mt-md rounded">
                   <q-item-section>
                     <q-item-label lines="1" class="q-mt-sm">
                       <span class="text-weight-medium"
